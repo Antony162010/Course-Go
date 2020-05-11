c7530fe2179e60e9c046513ef15caac93513b883
Antony Abel Montalvo Garcia <antonis162010@gmail.com>
2020-05-10 22:21:23 -0500

    Complete calculadora

--- main.go
+++ main.go
@@ -1,13 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello world.")
-	imprime()
+type calc struct {
 }
 
-func imprime() {
-	defer fmt.Println("Inicio")
-	fmt.Println("Final")
+func (calc) makeOperation(input string, operator string) int {
+	valores := strings.Split(input, operator)
+
+	operador1 := parseText(valores[0])
+	operador2 := parseText(valores[1])
+	switch operator {
+	case "+":
+		return operador1 + operador2
+	case "-":
+		return operador1 - operador2
+	case "*":
+		return operador1 * operador2
+	case "/":
+		return operador1 / operador2
+	default:
+		return 0
+	}
+}
+
+func parseText(input string) int {
+	operador, err1 := strconv.Atoi(input)
+
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	return operador
+}
+
+func readInput() string {
+	scaner := bufio.NewScanner(os.Stdin)
+	scaner.Scan()
+
+	return scaner.Text()
+}
+func main() {
+	operations := []string{"+", "-", "*", "/"}
+
+	fmt.Print("Ingresa tu suma: ")
+	operation := readInput()
+
+	operator := ""
+	for i := 0; i < len(operations); i++ {
+		if strings.Contains(operation, operations[i]) {
+			operator = operations[i]
+		}
+	}
+
+	c := calc{}
+	result := c.makeOperation(operation, operator)
+	fmt.Print("Resultado: ", result)
+
 }
